675cbecc4420d22e96c133f4e6609537f967cec4
Arjun Patil <arjunshivaji100@gmail.com>
2021-07-22 22:37:03 +0530

    Swap using xor operator

---
+++ BitSwap.java
@@ -0,0 +1,10 @@
+class BitSwap {
+    public static void main(String args[]) {
+        int a = 5, b = 10;
+        System.out.println("a = " + a + "\nb = " + b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a = " + a + "\nb = " + b);
+    }
+}
